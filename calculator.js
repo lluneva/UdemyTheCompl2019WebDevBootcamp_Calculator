7083aa24bdd43a4e5ea90ed2f0dc0315f417564e
Lidija Luneva <lidiaLuneva@gmail.com>
2018-12-27 13:29:24 +0200

    Initail commit

---
+++ calculator.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({
+  //izmantojot šo, var tikt klāt ievadītajiem datiem//
+  extended: true
+}));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+app.post("/", function (req, res) {
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+
+  var result = num1 + num2;
+  res.send("<h1>The result of calculation is:</h1> " + result);
+})
+
+
+// BMI calculator//
+
+app.get("/bmiCalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function (req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+
+  var bmiIndex = weight / (height * height);
+  if (bmiIndex <= 20) {
+    res.send("Your BMI index is below normal" + bmiIndex)
+  } else if (bmiIndex > 20 && bmiIndex <= 23) {
+    res.send("Your BMI index is nromal : " + bmiIndex)
+  } else {
+    res.send("Your BMI index is too high: " + bmiIndex)
+  }
+
+})
+
+app.listen(3000, function () {
+  console.log("Server started at port 3000");
+})
